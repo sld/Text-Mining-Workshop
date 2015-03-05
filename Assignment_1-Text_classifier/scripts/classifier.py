59f7b0c06bedd811dc063280c1d13bc3857177ea
sld <sld7700@gmail.com>
2015-03-05 12:59:42 +0300

    Some updates in classifier.py. Added README.md.

--- Assignment_1-Text_classifier/scripts/classifier.py
+++ Assignment_1-Text_classifier/scripts/classifier.py
@@ -2,6 +2,7 @@ import pickle
 
 from sys import argv
 from collections import defaultdict
+from random import shuffle
 
 from sqlite_wrapper import SqliteWrapper
 from sklearn.svm import LinearSVC
@@ -21,6 +22,7 @@ class BaseClassifier:
         self._test_data_filled = False
 
     def _split(self, data):
+        shuffle(data)
         sents = [e[0] for e in data]
         labels = [e[1] for e in data]
         return sents, labels
@@ -49,19 +51,43 @@ class BaseClassifier:
 
 class StreamClassifier(BaseClassifier):
     def pipeline(self, max_i=400, batch_size=5000, test_iteration=20):
-        classifier_prerformance = []
         self._init_classifier()
-        self._online_train_and_test(max_i, batch_size, test_iteration)
+        start_i = 0
+        # start_i, max_i = self._load_classifier_from_file(60)
+        print(start_i, max_i)
+        self._online_train_and_test(start_i, max_i, batch_size, test_iteration)
+
+    def overall_test(self, classifier_ind=320):
+        self._load_classifier_from_file(classifier_ind)
+
+        query = "SELECT * FROM test_sents WHERE label=? ORDER BY sent LIMIT ? OFFSET ?"
+        test_data = self._get_batch(0, 200000, query, 2)
+        test_sents, test_labels = self._split(test_data)
+        test_transformed_sents = self.vectorizer.transform(test_sents)
+
+        predicted = self.classifier.predict(test_transformed_sents)
+        return accuracy_score(test_labels, predicted), f1_score(test_labels, predicted)
 
     def _init_classifier(self):
-        self.vectorizer = HashingVectorizer(ngram_range=(1, 3),
+        self.vectorizer = HashingVectorizer(ngram_range=(1, 2),
                                             non_negative=True,
-                                            stop_words=self._get_stop_words())
+                                            stop_words=self._get_stop_words(),
+                                            lowercase=False,
+                                            decode_error='ignore')
         self.classifier = MultinomialNB()
+        # self.classifier = SGDClassifier(n_jobs=-1)
 
-    def _online_train_and_test(self, max_i, batch_size, test_iteration):
-        classifier_prerformance = []
-        for i in range(0, max_i):
+    def _load_classifier_from_file(self, ind):
+        data = pickle.load(open('../states-multinomial-nb/stream_classifier_{0}.p'.format(ind), 'rb'))
+        self.classifier = data[0]
+        self.vectorizer = data[1]
+        start_i = data[2]
+        max_i = data[3]
+        return start_i, max_i
+
+    def _online_train_and_test(self, start_i, max_i, batch_size, test_iteration):
+        self.classifier_prerformance = []
+        for i in range(start_i, max_i):
             batch = self._get_batch(i, batch_size)
             sents, labels = self._split(batch)
             transformed_sents = self.vectorizer.transform(sents)
@@ -69,14 +95,17 @@ class StreamClassifier(BaseClassifier):
             self.classifier.partial_fit(transformed_sents, labels, classes=self._labels)
 
             if i % test_iteration == 0:
-                scores = self._test_classifier(i)
-                pickle.dump((self.classifier, self.vectorizer, i, max_i),
-                            open('../states/stream_classifier_{0}.p'.format(i), 'wb'))
+                self._test_and_dump(i, max_i, batch_size)
 
-                classifier_prerformance.append((i, batch_size, scores))
-                pickle.dump(classifier_prerformance,
-                            open('../states/overall_stream_classifier_performance.p', 'wb'))
-                print((i, batch_size, scores))
+    def _test_and_dump(self, i, max_i, batch_size):
+        scores = self._test_classifier(i)
+        pickle.dump((self.classifier, self.vectorizer, i, max_i),
+                    open('../states/stream_classifier_{0}.p'.format(i), 'wb'))
+
+        self.classifier_prerformance.append((i, batch_size, scores))
+        pickle.dump(self.classifier_prerformance,
+                    open('../states/overall_stream_classifier_performance.p', 'wb'))
+        print((i, batch_size, scores))
 
     def _test_classifier(self, i):
         if self._test_data_filled is not True:
@@ -93,9 +122,8 @@ class StreamClassifier(BaseClassifier):
 class InMemoryClassifier(BaseClassifier):
     def pipeline(self, size=100000):
         train = self._get_batch(0, size)
-        test = self._get_batch(1, size)
         self.train_classifier(train)
-        print(self.test_classifier(test))
+        print(self._test_classifier())
 
     def train_classifier(self, data):
         sents, labels = self._split(data)
@@ -103,38 +131,25 @@ class InMemoryClassifier(BaseClassifier):
                                                            non_negative=True,
                                                            stop_words=self._get_stop_words())),
                               ('tfidf', TfidfTransformer()),
+                              ('feature_selection', LinearSVC(penalty="l1", dual=False)),
                               ('classifier',  LinearSVC())])
         pipeline_.fit(sents, labels)
-        self.classifier_pipeline = pipeline_
+        self.classifier = pipeline_
 
-    def test_classifier(self, data):
-        sents, labels = self._split(data)
-        predicted = self.classifier_pipeline.predict(sents)
-        return accuracy_score(labels, predicted), f1_score(labels, predicted)
+    def _test_classifier(self):
+        query = "SELECT * FROM test_sents WHERE label=? ORDER BY sent LIMIT ? OFFSET ?"
+        test_data = self._get_batch(0, 10000, query, 2)
+        test_sents, test_labels = self._split(test_data)
 
-    def _load_from_file(self, size):
-        return pickle.load(open('../train_and_test_{0}.p'.format(size), 'rb'))
-
-    def _train_and_test_sets(self, limit=300000):
-        data = {}
-        for label in self._labels:
-            self.sqlite_wrapper.execute("SELECT * FROM sents WHERE label=? ORDER BY RANDOM() LIMIT ?",
-                                        (label, limit))
-            data[label] = list(self.sqlite_wrapper.executor)
-
-        train_data = []
-        test_data = []
-        cnt80 = int(0.8*limit)
-        for label, sents in data.items():
-            classifier_data = [(self._features(sent), label) for _, sent in sents]
-            train_data += classifier_data[:cnt80]
-            test_data += classifier_data[cnt80:]
-        return (train_data, test_data)
+        predicted = self.classifier.predict(test_sents)
+        return accuracy_score(test_labels, predicted), f1_score(test_labels, predicted)
 
 
 if __name__ == '__main__':
     # cl_in_memory = InMemoryClassifier()
-    # cl_in_memory.pipeline(100000)
+    # cl_in_memory.pipeline(20000)
 
     cl_stream = StreamClassifier()
-    cl_stream.pipeline(1035, 2500)
+    # cl_stream.pipeline(10350, 250, 100)
+    # 320
+    print(cl_stream.overall_test(320))
